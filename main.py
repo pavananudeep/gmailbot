f99e867017d6e62d0f0ac6817aa0e212ba0bc431
pavananudeep <66690971+pavananudeep@users.noreply.github.com>
2021-11-07 09:03:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import pyautogui
-from time import sleep
 import os
 import webbrowser
+import pyautogui as pt
+import pyperclip
+from time import sleep
+
+webbrowser.open("https://accounts.google.com/AddSession/identifier?hl=en-GB&continue=https%3A%2F%2Fmail.google.com&service=mail&ec=GAlAFw&flowName=GlifWebSignIn&flowEntry=AddSession")
+sleep(5)
+position=pt.locateOnScreen("email.PNG",confidence=0.6)
+x=position[0]
+y=position[1]
+pt.moveTo(x+10,y+27)
+pt.click()
+pt.typewrite("pavananudeepmotiki@gmail.com")
+pt.typewrite("\n")
+sleep(5)
+pt.typewrite("Manjula@9")
+pt.typewrite("\n")
